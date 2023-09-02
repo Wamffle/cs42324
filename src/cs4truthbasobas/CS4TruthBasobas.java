e01a1edcb3057c9e548dfa7cd2e99d958b330120
Wamffle <113017291+Wamffle@users.noreply.github.com>
2023-09-02 22:40:28 +0800

    Update CS4TruthBasobas.java

--- src/cs4truthbasobas/CS4TruthBasobas.java
+++ src/cs4truthbasobas/CS4TruthBasobas.java
@@ -4,12 +4,10 @@ package cs4truthbasobas;
  *
  * @author TRUTH
  */
-public class CS4TruthBasobas {
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
         //nickname, age, main hobby, snack preference
         
-        String sib1 = "Yvette Basobas";
+        String sib1 = "Yvette Basobas ";
         int age1 = 19;
         String hob1 = "Reading";
         var snackPref1 = "Savory"; //Do they prefer a sweet or a savory snack?
@@ -18,7 +16,7 @@ public class CS4TruthBasobas {
         System.out.println("Hobby: " + hob1);
         System.out.println("Snack preference(Sweet or Savory): " + snackPref1 + "\n");
         
-        String sib2 = "Sandy Basobas";
+        String sib2 = "Sandy Basobas ";
         int age2 = 15;
         String hob2 = "Binge-watching";
         var snackPref2 = "Sweet"; //Do they prefer a sweet or a savory snack?
@@ -27,7 +25,7 @@ public class CS4TruthBasobas {
         System.out.println("Hobby: " + hob2);
         System.out.println("Snack preference(Sweet or Savory): " + snackPref2 + "\n");
         
-        String sib3 = "Julia basobas";
+        String sib3 = "Julia basobas ";
         int age3 = 12;
         String hob3 = "Gaming";
         var snackPref3 = "Savory"; //Do they prefer a sweet or a savory snack?
@@ -40,9 +38,9 @@ public class CS4TruthBasobas {
         System.out.println("Difference between the ages of the eldest and youngest sibling: " + diffAges + " years"); //(operation: difference)
         boolean teen = age3 >= 13 && age1 <=19 ;
         System.out.println("The youngest sibling is already a teen: " + teen); //(operation: comparison)
-        boolean sameHobbies = hob1!=hob2 && hob1!=hob3 && hob2!=hob3;
-        System.out.println("The Basobas siblings have distinct hobbies: " + sameHobbies); //(operation: comparison and logical)
-        boolean sameSnackPref = snackPref2 == snackPref3;
+        boolean sameHobbies = (hob1.equals(hob3) || hob1.equals(hob2) || hob2.equals(hob3));
+        System.out.println("No two siblings have the same main hobby: " + !(sameHobbies)); //(operation: comparison and logical)
+        boolean sameSnackPref = snackPref2.equals(snackPref3);
         System.out.println("The middle and youngest sibling have similar snack preference: " + sameSnackPref); //(operation: comparison)
     }
     
